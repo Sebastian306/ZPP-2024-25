ef22eecb9dd9dbe739117e2870e3386f2a36b350
Sebastian <s.marczak2@student.uw.edu.pl>
2024-10-29 23:27:03 +0100

    Add terrain checker class

--- terrain-type-detection/terrainChecker.cpp
+++ terrain-type-detection/terrainChecker.cpp
@@ -1,8 +1,75 @@
 #include "terrainChecker.h"
 
-TerrainChecker::TerrainChecker(std::string tilesFolderPath, std::string tilesPropertiesFileName, std::string tileFileNameTemplate) {
-    this->tilesFolderPath = tilesFolderPath;
-    this->tilesPropertiesFileName = tilesPropertiesFileName;
-    this->tileFileNameTemplate = tileFileNameTemplate;
+TerrainChecker::TerrainChecker() {
+    tileSizeX = 3;
+    tileSizeY = 3;
+    tilesCountX = 121;
+    tilesCountY = 61;
+    shiftX = 180;
+    shiftY = 90;
 }
 
+std::pair<int, int> TerrainChecker::getTileForPoint(GeoPoint point) const {
+    int x = (point.getLat() + shiftX) / tileSizeX;
+    int y = (point.getLon() + shiftY) / tileSizeY;
+    return std::make_pair(x, y);
+}
+
+std::string TerrainChecker::getTileFileName(int x, int y) {
+    // using "tileFileNameTemplate = tile_{}_{}"
+    return TILES_FOLDER_PATH + std::format(TILES_FILE_NAME_TEMPLATE, x, y);
+}
+
+void TerrainChecker::readTile(int x, int y)  {
+    std::string filename = getTileFileName(x, y);
+    std::ifstream inFile(filename, std::ios::binary);
+    if (!inFile) {
+        std::cerr << "Error opening tile file for reading!. filename: " << filename << std::endl;
+    }
+
+    int polygonsCount;
+    inFile.read(reinterpret_cast<char*>(&polygonsCount), sizeof(polygonsCount));  // Odczyt liczby wierzchołków
+    std::vector<TerrainPolygon> polygons;
+    for (int i = 0; i < polygonsCount; ++i) {
+        int verticesCount;
+        inFile.read(reinterpret_cast<char*>(&verticesCount), sizeof(verticesCount));  // Odczyt liczby wierzchołków
+        std::vector<std::pair<double, double>> coords;
+        for (int j = 0; j < verticesCount; ++j) {
+            double lat, lon;
+            inFile.read(reinterpret_cast<char*>(&lat), sizeof(lat));  // Odczyt lat
+            inFile.read(reinterpret_cast<char*>(&lon), sizeof(lon));  // Odczyt lon
+            coords.push_back(std::make_pair(lat, lon));
+        }
+
+        polygons.push_back(TerrainPolygon(coords));
+    }
+    inFile.close();
+    
+    terrainMap[std::make_tuple(x, y)] = polygons;
+}
+
+std::vector<TerrainPolygon> TerrainChecker::getPolygonsForTile(int x, int y){
+    if(terrainMap.find(std::make_tuple(x, y)) == terrainMap.end()) {
+        readTile(x, y);
+        if(terrainMap.find(std::make_tuple(x, y)) == terrainMap.end()) {
+            return std::vector<TerrainPolygon>();
+        }
+    }
+    return terrainMap[std::make_tuple(x, y)];
+};
+
+std::vector<TerrainPolygon> TerrainChecker::getPolygonsForPoint(GeoPoint point){
+    std::pair<int, int> tile = getTileForPoint(point);
+    return getPolygonsForTile(tile.first, tile.second);
+}
+bool TerrainChecker::isOnLand(GeoPoint point){
+    std::vector<TerrainPolygon> polygons = getPolygonsForPoint(point);
+    // std::cout<<"Checking if point is on land: "<<point.getLat() + shiftX<<" "<<point.getLon() + shiftY<<std::endl;
+    for (auto polygon : polygons) {
+        // std::cout<<polygon.toString()<<std::endl;
+        if (polygon.isInside(point.getLat() + shiftX, point.getLon() + shiftY)) {
+            return true;
+        }
+    }
+    return false;
+}
--- terrain-type-detection/terrainChecker.h
+++ terrain-type-detection/terrainChecker.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <vector>
 #include <tuple>
+#include <format>
 #include "../common.h"
 #include "terrainPolygon.h"
 
@@ -13,24 +14,21 @@ class TerrainChecker
 {
     private:
         std::map<std::tuple<int, int>, std::vector<TerrainPolygon>> terrainMap;
-        std::string tilesFolderPath;
-        std::string tilesPropertiesFileName;
-        std::string tileFileNameTemplate;
         long double tileSizeX;
         long double tileSizeY;
         int tilesCountX;
         int tilesCountY;
         long double shiftX;
         long double shiftY;
+
+        void readTile(int x, int y);
     public:
-        TerrainChecker(
-            std::string tilesFolderPath = TILES_FOLDER_PATH, 
-            std::string tilesPropertiesFileName = TILES_PROPERTIES_FILE_NAME,
-            std::string tileFileNameTemplate = TILES_FILE_NAME_TEMPLATE
-            );
-        std::vector<TerrainPolygon> getPolygonsForTile(int x, int y) const;
-        std::vector<TerrainPolygon> getPolygonsForPoint(GeoPoint point) const;
-        bool isOnLand(GeoPoint point) const;
+        TerrainChecker();
+        std::string getTileFileName(int x, int y);
+        std::pair<int, int> getTileForPoint(GeoPoint point) const;
+        std::vector<TerrainPolygon> getPolygonsForTile(int x, int y);
+        std::vector<TerrainPolygon> getPolygonsForPoint(GeoPoint point);
+        bool isOnLand(GeoPoint point);
 };
 
 #endif // terrainChecker_H
