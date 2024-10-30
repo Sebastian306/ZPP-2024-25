553fe36d37e819d204df0d2033b904d83996dc86
Sebastian <s.marczak2@student.uw.edu.pl>
2024-10-30 23:53:47 +0100

    correct template

--- common.h
+++ common.h
@@ -4,7 +4,8 @@
 #define TERRAIN_TYPE_FOLDER_PATH "terrain-type-detection/"
 #define TILES_FOLDER_PATH "terrain-type-detection/tiles/"
 #define TILES_PROPERTIES_FILE_NAME "tiles.properties"
-#define TILES_FILE_NAME_TEMPLATE "tile_{}_{}" // {} will be replaced by x and y
+#define TILES_FILE_NAME_TEMPLATE "tile_{:d}_{:d}.txt" // {} will be replaced by x and y
+
 
 class GeoPoint {
     private:
